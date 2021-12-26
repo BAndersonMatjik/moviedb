69cf8443f122468441d86057d8128de3751e7043
billy anderson matjik <v-billy.matjik@anteraja.id>
2021-12-26 14:30:34 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/billyand/flutterproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.billyand.flutterproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,2 @@
+const String baseUrl="";
+const String baseApiKey ="5d3864bb0b7cb3607adaee91806d3c36";
