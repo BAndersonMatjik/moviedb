d2a5d1036fbb6320698993e95dd84c7566909288
billy anderson matjik <v-billy.matjik@anteraja.id>
2021-12-26 22:29:19 +0700

    Fix Null Pointer Fix

---
+++ lib/core/usecase/GetMovieUseCase.dart
@@ -0,0 +1,14 @@
+import 'dart:ffi';
+
+import 'package:flutterproject/core/model/movie.dart';
+import 'package:flutterproject/core/utils/usecase.dart';
+
+class GetMovieUseCase implements UseCase<Movie,Void>{
+  @override
+  Future<Movie> call({required Void params}) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+
+  
+}
